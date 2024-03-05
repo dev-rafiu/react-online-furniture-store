8788e6eb313f491057020336ef36dee976c688dd
Rafiw Idrissu <dev.rafiu@gmail.com>
2024-03-05 17:42:30 +0000

    feat: updates

---
+++ src/hooks/api/useFetchProducts.jsx
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+const endpoint = process.env.APIEndpoint;
+console.log(endpoint);
+
+async function fetchProducts() {
+  const response = await fetch();
+  return;
+}
+
+export default fetchProducts;
